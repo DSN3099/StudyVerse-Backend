11afc0d658753b9fa09b3d9ec6072864424dfbe8
ayushsingh0607 <ayushraj0607@gmail.com>
2023-02-20 20:51:08 +0530

    ayush commits uploding.

--- Routes/lesson.js
+++ Routes/lesson.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import {
-    getCourse,
     addVideos
 } from '../Controllers/lesson.js'
 const router = express.Router()
 
-router.get('/:id', getCourse)
 router.post('/:id',addVideos)
 
 export default router;
