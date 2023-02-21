8bc608564d68a94c693e3a3df435875aa497052f
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2023-02-21 14:01:33 +0530

    gaurav commits lesson.js

--- Routes/lesson.js
+++ Routes/lesson.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {
-    addVideos
+    addVideos, deleteVideo, editVideo
 } from '../Controllers/lesson.js'
 const router = express.Router()
 
 router.post('/:id',addVideos)
+router.delete('/:id',deleteVideo)
+router.patch('/:id',editVideo)
 
 export default router;
