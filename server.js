b9cbec296dc865cc3d617bc32caecf0496cffb99
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2023-03-28 15:06:21 +0530

    gaurav commits

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connection.on('connected', () => {
 })
 
 const corsOption = {
-    origin: 'https://studyverse.netlify.app',
+    origin: 'https://localhost:3000',
     credentials: true
 }
 
